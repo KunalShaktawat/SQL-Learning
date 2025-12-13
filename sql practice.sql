b8229c0bd8f5b3a6077433648a1ff10a0139a750
KunalShaktawat <kunalshaktawat97@gmail.com>
2025-12-13 14:50:04 +0530

    Day 02: Practiced Numeric and Date function

--- sql practice.sql
+++ sql practice.sql
@@ -28,4 +28,37 @@ select name, substr(name,2), substr(name,2,3) from country;
 select name, substring(name,3) from country;
 
 
+-- ===============================
+-- Day 02 : Numeric and Date function
+-- Date  : 13-Dec-2025
+-- ===============================
+
+-- numeric functions
+use world;
+select lifeexpectancy,round(lifeexpectancy) from country;
+select 83.69,round(83.687,2);
+-- hundred place,tenth place,once place
+-- round method
+select(456.23),round(456.23,-1);
+select(46.23),round(555.23,-2);
+-- truncate method
+select 566.56,truncate(566.56,1);
+-- mod
+select 43,3,mod(43,3);
+-- pow
+select pow(2,3);
+-- ciel and floor
+select ceil(3.000007),floor(3.999999);
+-- Date
+-- inbuilt date yh data and time print krna
+select now(),curdate(),current_timestamp(),current_time();
+-- add date
+select now(),adddate(now(),2), adddate(now(), interval 3 month);
+select now(),addtime(now(),20000);
+select now(), subdate(now(),2), subdate(now(), interval 4 month);
+select now(),year(now()),extract(WEEK from now());
+select now() ,date_format(now(),'Month is %M & year is %y');
+
+
+
 
